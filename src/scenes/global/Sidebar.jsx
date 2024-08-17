3788dd273e587e94ac6ee2c1dd74e31cff1d90fc
user.name : Dhruvan Patel <user.email - pateldhruvan52@gmail.com>
2024-08-17 22:23:19 +0530

    sidebar

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { Link } from 'react-router-dom';
+import { tokens } from '../../theme';
 
 const Sidebar = () => {
     return (
         <div>
-            
+            {/* Sidebar content goes here */}
         </div>
     );
 }
