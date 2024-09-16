76fc592045951034efd5629796255a9985d99af1
user.name : Dhruvan Patel <user.email - pateldhruvan52@gmail.com>
2024-09-16 18:22:47 +0530

    FAQ added

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import Team from "./scenes/team/Team";
 import Invoices from "./scenes/invoices/Invoices";
 import Contacts from "./scenes/contacts/Contacts";
 import Form from "./scenes/form/Form"
+import FAQ from "./scenes/FAQ/FAQ";
 // import Bar from "./scenes/bar";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
-// import FAQ from "./scenes/faq";
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
@@ -35,10 +35,10 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
+              <Route path="/faq" element={<FAQ />} />
               {/*<Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
               <Route path="/geography" element={<Geography />} /> */}
             </Routes>
           </main>
