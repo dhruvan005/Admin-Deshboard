479b2b0f6c4b4b4aa5f3adc72665f3dbf216b5e9
user.name : Dhruvan Patel <user.email - pateldhruvan52@gmail.com>
2024-09-16 16:43:42 +0530

    calendar done

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,7 @@ import Form from "./scenes/form/Form"
 // import Geography from "./scenes/geography";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-
-// import Calendar from "./scenes/calendar/calendar";
+import Calendar from "./scenes/calendar/Calendar";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -29,19 +28,19 @@ function App() {
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
-             <Routes>
+            <Routes>
               <Route path="/" element={<Dashboard />} />
-               <Route path="/team" element={<Team />} />
-             <Route path="/contacts" element={<Contacts />} />
-             <Route path="/invoices" element={<Invoices />} />
-               <Route path="/form" element={<Form />} />
-               {/*<Route path="/bar" element={<Bar />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/calendar" element={<Calendar />} />
+              {/*<Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
               <Route path="/faq" element={<FAQ />} />
-              <Route path="/calendar" element={<Calendar />} />
               <Route path="/geography" element={<Geography />} /> */}
-            </Routes> 
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
